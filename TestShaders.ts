5b95c9a1d1ac7679f0f24308bc4496d8972b6961
Lindsay Victoria Abbott <IzzieAugustProductions@gmail.com>
2026-06-26 11:27:13 -0400

    Fixed attributes for cube, adjusted pos and scale

--- TestShaders.ts
+++ TestShaders.ts
@@ -1,3 +1,4 @@
+import { Color } from "./Yuu API/Basic Types/Color";
 import { Quaternion } from "./Yuu API/Basic Types/Quaternion";
 import { Vector3 } from "./Yuu API/Basic Types/Vector3";
 import { registerStart } from "./Yuu API/RegisterStart";
@@ -11,7 +12,7 @@ import { spawnPrimitive } from "./Yuu API/SpawnPrimitive";
 registerStart(start);
 function start() {
 
-    spawnPrimitive.plane("Front", new Vector3(0, 3, 2), new Vector3(3, 1, 1), Quaternion, new Vector3(0, 0, 0), 1, true, "Static", undefined)
+    spawnPrimitive.plane("Front", new Vector3(1, 3, 2), new Vector3(1, 3, 1), new Quaternion, new Color(0, 0, 0), 1, "Convex", "Static", undefined)
 
 
 }
